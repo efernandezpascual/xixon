3dbfb2f5e0f90a823eb118b6a98d069f6d9d4f10
Eduardo Fernández Pascual <61799380+efernandezpascual@users.noreply.github.com>
2025-09-30 15:24:06 +0200

    accepted submission

---
+++ src/5-biodiversity-SEBOT.R
@@ -0,0 +1,242 @@
+library(tidyverse); library(ggvenn); library(vegan)
+
+read.csv("data/header.csv", fileEncoding = "latin1") -> header
+read.csv("data/plants.csv", fileEncoding = "latin1") -> plants
+read.csv("data/fungi.csv", fileEncoding = "latin1") -> fungi
+read.csv("data/bacteria.csv", fileEncoding = "latin1") -> bacteria
+read.csv("data/soils.csv", fileEncoding = "latin1") -> soils
+read.csv("data/soil-pca.csv", fileEncoding = "latin1") -> pca
+
+### NMDS
+
+c("forestgreen",  
+  "gold", 
+  "#40E0D0",
+  "#A020F0",
+  "#5CACEE",
+  "#551A8B") -> colores
+
+### NMDS plant communities
+
+header %>%
+  select(habitat, id) %>%
+  merge(plants) %>%
+  select(id, taxon, cover) %>%
+  mutate(cover = 1) %>% # Transform cover to presence
+  spread(taxon, cover, fill = 0) %>%
+  column_to_rownames(var = "id") -> matrix 
+
+matrix %>%
+  vegan::metaMDS(trymax = 100000, k = 2) -> 
+  nmds # Ordination output
+
+### Site scores
+
+vegan::scores(nmds, "sites") %>%
+  data.frame() %>%
+  rownames_to_column("id") %>%
+  rename(Dim.1 = NMDS1, Dim.2 = NMDS2) %>%
+  merge(header) %>%
+  dplyr::select(id, habitat, Dim.1, Dim.2) %>%
+  mutate(habitat = fct_relevel(habitat, "forest", "meadow", "park", "roadside", "residential", "industrial")) %>%
+  mutate(habitat = fct_recode(habitat, "BOSQUES" = "forest", "PRADOS" = "meadow",
+                              "PARQUES" = "park", "CUNETAS" = "roadside",
+                              "RESIDENCIALES" = "residential", "INDUSTRIALES" = "industrial")) -> sitescores
+
+### Species scores
+
+vegan::scores(nmds, "species") %>%
+  data.frame() %>%
+  rownames_to_column("Taxon") %>%
+  rename(Dim.1 = NMDS1, Dim.2 = NMDS2) %>%
+  mutate(Species = Taxon) %>%
+  separate(Species, into = c("G", "S")) %>%
+  mutate(Species = paste(substr(G, 1, 3), substr(S, 1, 3), sep = "")) %>%
+  dplyr::select(Taxon, Species, Dim.1, Dim.2) -> sppscores
+
+### Plot NMDS sites
+
+ggplot(sitescores, aes(x = Dim.1, y = Dim.2)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  geom_point(aes(fill = habitat), color = "black", size = 3.5, shape = 21) +
+  #ggrepel::geom_text_repel(data = sppscores, aes(label = Species), color = "grey33", fontface = "italic", size = 3.5, segment.color = "transparent") +
+  ggthemes::theme_tufte() +
+  ggtitle(label = "PLANTAS") +
+  theme(text = element_text(family = "sans"),
+        strip.background = element_blank(),
+        legend.position = "none", 
+        #legend.direction = "vertical",
+        legend.title = element_text(size = 10),
+        legend.spacing.x = unit(0, "mm"),
+        legend.spacing.y = unit(0, "mm"),
+        legend.text = element_text(size = 14), 
+        panel.background = element_rect(color = "black", fill = NULL),
+        strip.text = element_text(size = 18, hjust = 0, margin = margin(l = 0, b = 4)),
+        #strip.text = element_blank(),
+        plot.title = element_text(size = 18),
+#        axis.title = element_blank(),
+        #axis.title.y = element_text(size = 10),
+        axis.text.x = element_text(size = 10, color = "black"),
+        axis.text.y = element_text(size = 10, color = "black"),
+        plot.margin = unit(c(.5,.5,.5,.5), "cm")) +
+  scale_x_continuous(name = "Dimensión 1", labels = scales::number_format(accuracy = 0.1),
+                     breaks = seq(-2, 2, by = 0.5)) + 
+  scale_y_continuous(name = "Dimensión 2", labels = scales::number_format(accuracy = 0.1),
+                     breaks = seq(-2, 2, by = 0.5)) +
+  scale_color_manual(values = colores) +
+  scale_fill_manual(values = colores) -> F3D; F3D
+
+### NMDS fungal communities
+
+fungi %>%
+  select(id, ASV, presence) %>%
+  spread(ASV, presence, fill = 0) %>%
+  column_to_rownames(var = "id") -> matrix 
+
+matrix %>%
+  vegan::metaMDS(trymax = 100000, k = 2) -> 
+  nmds # Ordination output
+
+### Site scores
+
+vegan::scores(nmds, "sites") %>%
+  data.frame() %>%
+  rownames_to_column("id") %>%
+  rename(Dim.1 = NMDS1, Dim.2 = NMDS2) %>%
+  merge(header) %>%
+  dplyr::select(id, habitat, Dim.1, Dim.2) %>%
+  mutate(habitat = fct_relevel(habitat, "forest", "meadow", "park", "roadside", "residential", "industrial")) %>%
+  mutate(habitat = fct_recode(habitat, "BOSQUES" = "forest", "PRADOS" = "meadow",
+                              "PARQUES" = "park", "CUNETAS" = "roadside",
+                              "RESIDENCIALES" = "residential", "INDUSTRIALES" = "industrial")) -> sitescores
+
+### Species scores
+
+vegan::scores(nmds, "species") %>%
+  data.frame() %>%
+  rownames_to_column("Taxon") %>%
+  rename(Dim.1 = NMDS1, Dim.2 = NMDS2) %>%
+  mutate(Species = Taxon) %>%
+  dplyr::select(Taxon, Species, Dim.1, Dim.2) -> sppscores
+
+### Plot NMDS sites
+
+ggplot(sitescores, aes(x = Dim.1, y = Dim.2)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  geom_point(aes(fill = habitat), color = "black", size = 3.5, shape = 21) +
+  #ggrepel::geom_text_repel(data = sppscores, aes(label = Species), color = "grey33", fontface = "italic", size = 3.5, segment.color = "transparent") +
+  ggthemes::theme_tufte() +
+  ggtitle(label = "HONGOS") +
+  theme(text = element_text(family = "sans"),
+        strip.background = element_blank(),
+        legend.position = "none", 
+        #legend.direction = "vertical",
+        legend.title = element_text(size = 10),
+        legend.spacing.x = unit(0, "mm"),
+        legend.spacing.y = unit(0, "mm"),
+        legend.text = element_text(size = 14), 
+        panel.background = element_rect(color = "black", fill = NULL),
+        strip.text = element_text(size = 18, hjust = 0, margin = margin(l = 0, b = 4)),
+        #strip.text = element_blank(),
+        plot.title = element_text(size = 18),
+#        axis.title = element_blank(),
+        #axis.title.y = element_text(size = 10),
+        axis.text.x = element_text(size = 10, color = "black"),
+        axis.text.y = element_text(size = 10, color = "black"),
+        plot.margin = unit(c(.5,.5,.5,.5), "cm")) +
+  scale_x_continuous(name = "Dimensión 1", labels = scales::number_format(accuracy = 0.1),
+                     breaks = seq(-2, 2, by = 1)) + 
+  scale_y_continuous(name = "Dimensión 2", labels = scales::number_format(accuracy = 0.1),
+                     breaks = seq(-2, 2, by = 0.5)) +
+  scale_color_manual(values = colores) +
+  scale_fill_manual(values = colores) -> F3E; F3E
+
+### NMDS bacterial communities
+
+bacteria %>%
+  select(id, ASV, presence) %>%
+  spread(ASV, presence, fill = 0) %>%
+  column_to_rownames(var = "id") -> matrix 
+
+matrix %>%
+  vegan::metaMDS(trymax = 100000, k = 2) -> 
+  nmds # Ordination output
+
+### Site scores
+
+vegan::scores(nmds, "sites") %>%
+  data.frame() %>%
+  rownames_to_column("id") %>%
+  rename(Dim.1 = NMDS1, Dim.2 = NMDS2) %>%
+  merge(header) %>%
+  dplyr::select(id, habitat, Dim.1, Dim.2) %>%
+  mutate(habitat = fct_relevel(habitat, "forest", "meadow", "park", "roadside", "residential", "industrial")) %>%
+  mutate(habitat = fct_recode(habitat, "BOSQUES" = "forest", "PRADOS" = "meadow",
+                              "PARQUES" = "park", "CUNETAS" = "roadside",
+                              "RESIDENCIALES" = "residential", "INDUSTRIALES" = "industrial")) -> sitescores
+
+### Species scores
+
+vegan::scores(nmds, "species") %>%
+  data.frame() %>%
+  rownames_to_column("Taxon") %>%
+  rename(Dim.1 = NMDS1, Dim.2 = NMDS2) %>%
+  mutate(Species = Taxon) %>%
+  separate(Species, into = c("G", "S")) %>%
+  mutate(Species = paste(substr(G, 1, 3), substr(S, 1, 3), sep = "")) %>%
+  dplyr::select(Taxon, Species, Dim.1, Dim.2) -> sppscores
+
+### Plot NMDS sites
+
+ggplot(sitescores, aes(x = Dim.1, y = Dim.2)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  geom_point(aes(fill = habitat), color = "black", size = 3.5, shape = 21) +
+  #ggrepel::geom_text_repel(data = sppscores, aes(label = Species), color = "grey33", fontface = "italic", size = 3.5, segment.color = "transparent") +
+  ggthemes::theme_tufte() +
+  ggtitle(label = "BACTERIAS") +
+  theme(text = element_text(family = "sans"),
+        strip.background = element_blank(),
+        legend.position = "none", 
+        #legend.direction = "vertical",
+        legend.title = element_text(size = 10),
+        legend.spacing.x = unit(0, "mm"),
+        legend.spacing.y = unit(0, "mm"),
+        legend.text = element_text(size = 14), 
+        panel.background = element_rect(color = "black", fill = NULL),
+        strip.text = element_text(size = 18, hjust = 0, margin = margin(l = 0, b = 4)),
+        #strip.text = element_blank(),
+        plot.title = element_text(size = 18),
+#        axis.title = element_blank(),
+        #axis.title.y = element_text(size = 10),
+        axis.text.x = element_text(size = 10, color = "black"),
+        axis.text.y = element_text(size = 10, color = "black"),
+        plot.margin = unit(c(.5,.5,.5,.5), "cm")) +
+  scale_x_continuous(name = "Dimensión 1", labels = scales::number_format(accuracy = 0.1),
+                     breaks = seq(-2, 2, by = 0.5)) + 
+  scale_y_continuous(name = "Dimensión 2", labels = scales::number_format(accuracy = 0.1),
+                     breaks = seq(-.3, .3, by = 0.3)) +
+  scale_color_manual(values = colores) +
+  scale_fill_manual(values = colores) -> F3F; F3F
+
+### Legend
+
+cowplot::get_plot_component(F3D + 
+                      theme(legend.position = "bottom", 
+                            legend.box.margin = margin(1, 0, 0, 0),
+                            legend.title = element_blank()) +
+                        guides(fill = guide_legend(nrow = 1)),
+                      "guide-box-bottom") -> legend
+
+### Merge
+
+cowplot::plot_grid(F3D, F3E, F3F, nrow = 1) -> F3DEF; F3DEF
+cowplot::plot_grid(F3DEF, legend, nrow = 2,
+                   rel_heights = c(1, .1)) -> F3DEFL; F3DEFL
+
+ggsave(F3DEFL, file = "results/figures/Fig3-SEBOT.png", bg = "white", 
+       path = NULL, scale = 1, width = 300, height = 140, units = "mm", dpi = 600)
+
+
